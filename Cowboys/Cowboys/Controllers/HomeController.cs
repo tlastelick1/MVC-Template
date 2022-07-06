3ab076db9284854a98545a9cf245d59eb3866185
tlastelick1 <75869801+tlastelick1@users.noreply.github.com>
2022-07-06 18:58:55 -0500

    Add files via upload

---
+++ Cowboys/Cowboys/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Cowboys.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cowboys.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult BuySeats()
+        {
+            ViewBag.FV = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult BuySeats(SeatPrices model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV = model.CalculateSeatPrice();
+            }
+            else
+            {
+                ViewBag.FV = 0;
+            }
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Cowboys/Cowboys/Models/SeatPrices.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;    // Needed by validation syntax 
+
+
+namespace Cowboys.Models
+{
+    public class SeatPrices
+    {
+        [Required(ErrorMessage =
+        "Please enter a section number.")]
+        [Range(101, 460, ErrorMessage =
+        "Section number must be between 101 and 460.")]
+        public int Seat { get; set; }
+        
+        public decimal? CalculateSeatPrice()
+        {
+            switch(Seat)
+            {
+                case int n when (n >= 101 && Seat <= 199):
+                    Seat = Seat + 437;
+                    break;
+
+                case int n when (n >= 200 && n <= 299):
+                    Seat = Seat + 212;
+                    break;
+
+                case int n when (n >= 300 && n <= 399):
+                    Seat = Seat - 59;
+                    break;
+
+                case int n when (n >= 400 && n <= 460):
+                    Seat = Seat - 255;
+                    break;
+
+            }
+            return Seat;
+        }
+
+    }
+}
